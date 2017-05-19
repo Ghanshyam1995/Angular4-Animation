ac8619b4051240af853d207e35660aabe17ceaff
Nikhil <nikhil.ithape@wwindia.com>
2017-05-19 20:46:03 +0530

    Initital Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import {fadeInAmimation} from "../animations/Route-animation";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[fadeInAmimation],
+  host:{'@fadeInAmimation':''}
+
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppRoutingModule } from './app-routing.module';
+import {MaterialModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import {Routes,RouterModule} from "@angular/router";
+import { EditComponent } from './edit/edit.component';
 
+const appRoutes : Routes =[
+  {path:'',redirectTo:'Home',pathMatch:'full'},
+   {path:'Home',component:UserComponent},
+   {path:'Edit',component:EditComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,MaterialModule,BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule,
-    HttpModule,
-    AppRoutingModule
+    HttpModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
